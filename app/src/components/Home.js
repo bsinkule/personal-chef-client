1ac52ff0bdf1f1231e6826008da298bb930ff2a8
Brice Sinkule <bsinkule@gmail.com>
2019-01-22 08:29:51 -0700

    3rd net form and auth prod check

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -3,14 +3,17 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <form name="submitMeme" method="POST" data-netlify="true">
+      <form name="submitMeme" netlify-honeypot="bot-field" method="POST" data-netlify="true">
+        <p style={{ display: "none" }}>
+          <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+        </p>
         <p>
           <label>Name: <input type="text" name="name"/></label>
         </p>
         <p>
           <label>Email: <input type="text" name="email"/></label>
         </p>
-        <div data-netlify-recaptcha="true"></div>
+        {/* <div data-netlify-recaptcha="true"></div> */}
         <p>
           <button type="submit">Send</button>
         </p>
