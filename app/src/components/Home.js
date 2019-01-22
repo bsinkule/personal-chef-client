323c34126bd34678425bea467398cba513b85ed8
Brice Sinkule <bsinkule@gmail.com>
2019-01-22 07:39:02 -0700

    test netlify forms

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home
+      <form name="submitMeme" netlify>
+        <p>
+          <label>Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Email: <input type="text" name="email"/></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   )
 }
