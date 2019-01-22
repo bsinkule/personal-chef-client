401d53aa7f9676a5d0d17526ae992947b071d394
Brice Sinkule <bsinkule@gmail.com>
2019-01-22 08:01:08 -0700

    2nd net form test

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -3,13 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <form name="submitMeme" netlify>
+      <form name="submitMeme" method="POST" data-netlify="true">
         <p>
           <label>Name: <input type="text" name="name"/></label>
         </p>
         <p>
           <label>Email: <input type="text" name="email"/></label>
         </p>
+        <div data-netlify-recaptcha="true"></div>
         <p>
           <button type="submit">Send</button>
         </p>
