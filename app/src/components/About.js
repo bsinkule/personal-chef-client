f6775a7fbeed327077fb946591e7ebcc8486143d
Brice Sinkule <bsinkule@gmail.com>
2019-01-24 22:50:36 -0700

    commit after install

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+import styled from 'styled-components';
+
+const MainWrapper = styled.div`
+  .aboutTop {
+    display: flex;
+    flex-direction: column;
+  }
+  .anch {
+    height: 500px;
+    margin: 5px 0px;
+    padding: 5px;
+  }
+  .one {
+    background-color: red;
+  }
+  .two {
+    background-color: yellow;
+  }
+  .three {
+    background-color: brown;
+  }
+  .four {
+    background-color: blue;
+  }
+`;
 
 const About = (props) => {
   console.log("about props: ", props)
   return (
-    <div>
-      About
-      <Link to="/home">Back</Link>
-    </div>
+    <MainWrapper>
+      <div className="aboutTop">
+        About
+        <Link to="/home">Back</Link>
+        <AnchorLink offset='50' href='#one'>section 1</AnchorLink>
+        <AnchorLink offset='50' href='#two'>section 2</AnchorLink> 
+        <AnchorLink offset='50' href='#three'>section 3</AnchorLink>
+        <AnchorLink offset='50' href='#four'>section 4</AnchorLink> 
+      </div>
+      <div className="one anch" id="one"> 
+        <div>One</div>
+      </div>
+      <div className="two anch" id="two"> 
+        <div>Two</div>
+      </div>
+      <div className="three anch" id="three"> 
+        <div>Three</div>
+      </div>
+      <div className="four anch" id="four"> 
+        <div>Four</div>
+      </div>
+
+    </MainWrapper>
   )
 }
 
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -3,7 +3,21 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home    
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>Your Name: <input type="text" name="name"/></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email"/></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   )
 }
