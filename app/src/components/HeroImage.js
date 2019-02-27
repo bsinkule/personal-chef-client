1b431385a2d916577ab1c69fc360f71d0b43d4db
Brice Sinkule <bsinkule@gmail.com>
2019-02-27 10:41:45 -0700

    changed hero image

--- app/src/components/HeroImage.js
+++ app/src/components/HeroImage.js
@@ -5,8 +5,7 @@ import styled from 'styled-components';
 
 const MainWrapper = styled.div`
   .hero-image {
-    background-image: linear-gradient(rgba(255, 245, 230, 0.1), rgba(255, 245, 230, 0.2)), url("https://agc.creativelive.com/agc/courses/5138-1.jpg");
-    // background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url("https://images.unsplash.com/photo-1541529086526-db283c563270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60");
+    background-image: linear-gradient(rgba(255, 245, 230, 0.4), rgba(255, 245, 230, 0.3)), url("https://images.unsplash.com/photo-1543353071-10c8ba85a904?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80");
     height: 600px;
     background-position: center;
     background-repeat: no-repeat;
@@ -14,26 +13,15 @@ const MainWrapper = styled.div`
     position: relative;
   }
 
-
   .hero-text {
     text-align: center;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    // color: white;
-
-      h1 {
-        font-size: 40px;
-        // background-color: rgba(255, 245, 230, .9);
-        background-color: rgba(242, 242, 242, .85);
-        padding: 35px 25px;
-        border-radius: 30px;
-      }
 
       p {
-        // background-color: rgba(255, 245, 230, .9);
-        background-color: rgba(242, 242, 242, .85);
+        background-color: rgba(242, 242, 242, .9);
         padding: 10px;
         border-radius: 20px;
         font-size: 18px;
@@ -62,6 +50,7 @@ const HeroImage = () => {
     <MainWrapper>
       <div className="hero-image">
         <div className="hero-text">
+          <img src="https://img.icons8.com/wired/2x/food-and-wine.png" alt="logo" />
           <h1>Denver's Best Dinner Party Duo</h1>
           <p>"Amazing flavors, unique items, Kyle and Con are so much fun to work with!" - Beth and Michael Ward</p>
           {/* <button><Link to="/Contact" className="Link">contact us</Link></button> */}
